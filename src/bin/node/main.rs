be68eb6e523ad7d583441a38a5922fa35cae1a9a
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-05-19 08:14:25 -0500

    working on rooms and signaling

--- src/bin/node/main.rs
+++ src/bin/node/main.rs
@@ -1,7 +1,15 @@
+mod rooms;
+mod socket;
+
+use socket::SocketData;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use structopt::StructOpt;
+use tokio::sync::RwLock;
+
+use warp::ws::WebSocket;
 use warp::Filter;
 use warp_reverse_proxy::reverse_proxy_filter;
-
-use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub struct HolAPI {
@@ -20,14 +28,23 @@ pub struct HolAPI {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = HolAPI::from_args();
 
-    let proxy_filter = reverse_proxy_filter(
+    let proxy = reverse_proxy_filter(
         "".to_string(),
         format!("http://localhost:{}", opt.urbit_port),
     );
 
-    let rooms_route = warp::path!("apps" / "rooms-v2").map(|| "This is the apps/rooms-v2 endpoint");
+    // let rooms_route = warp::path!("apps" / "rooms-v2").map(|| "This is the apps/rooms-v2 endpoint");
+    let socket_map: Arc<RwLock<HashMap<String, Mutex<WebSocket>>>> =
+        Arc::new(RwLock::new(HashMap::new()));
+    let queued_signals: Arc<RwLock<HashMap<String, Vec<SocketData>>>> =
+        Arc::new(RwLock::new(HashMap::new()));
 
-    let routes = rooms_route.or(proxy_filter);
+    let rooms_route = rooms::rooms_route();
+    let socket_route = socket::socket_route(Arc::clone(&socket_map), Arc::clone(&queued_signals));
+
+    let routes = rooms_route.or(socket_route).or(proxy);
+
+    // let routes = rooms_route.or(proxy);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], opt.node_port))
